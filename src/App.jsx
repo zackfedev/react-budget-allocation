bbb63fdc4480d6d6daf58e4c93803c6d8e0ad9d4
azzakyandhika <zakiiws22@gmail.com>
2023-03-14 21:42:54 +0700

    modified default react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useContext } from "react";
+import { AppContext, AppProvider } from "./context/AppContext";
+import Allocation from "./components/Allocation";
+import BudgetSpend from "./components/BudgetSpend";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div className='px-5'>
+        <h1>Budget Alocation App</h1>
+        <AppProvider>
+          <div className='container-fluid'>
+            <BudgetSpend />
+          </div>
+          <h2>Allocation</h2>
+          <div>
+            <Allocation />
+          </div>
+        </AppProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
