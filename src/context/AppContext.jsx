326df0afb45318b8ce1c9037f5afc8484e36cf4b
azzakyandhika <zakiiws22@gmail.com>
2023-03-17 12:01:48 +0700

    modified context

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,23 +1,54 @@
 import { useReducer, createContext } from "react";
 
 export const AppReducer = (state, action) => {
-  const state_updt = [];
+  let state_updt = [];
   switch (action.type) {
     case "ADD_ALLOCATION":
       state.allocation.map((item) => {
-        if (item.nameDPT === action.nameDPT) item.quantity = item.quantity + action.payload;
+        if (item.nameDPT === action.payload.nameDPT) item.quantity += action.payload.value;
+
         state_updt.push(item);
       });
       state.allocation = state_updt;
+      action.type = "DONE";
       return {
         ...state,
       };
     case "RED_ALLOCATION":
       state.allocation.map((item) => {
-        if (item.nameDPT === action.nameDPT) item.quantity = item.quantity - action.payload;
+        if (
+          item.nameDPT === action.payload.nameDPT &&
+          item.quantity !== 0 &&
+          action.payload.value <= item.quantity
+        ) {
+          item.quantity -= action.payload.value;
+        } else {
+          item.quantity = 0;
+        }
         state_updt.push(item);
       });
       state.allocation = state_updt;
+      action.type = "DONE";
+      return {
+        ...state,
+      };
+    case "DELETE":
+      state.allocation.map((item) => {
+        if (item.nameDPT === action.payload.nameDPT) item.quantity = 0;
+
+        state_updt.push(item);
+      });
+      state.allocation = state_updt;
+      action.type = "DONE";
+      return {
+        ...state,
+      };
+    case "SET_BUDGET":
+      if (parseInt(state.budget) === state.spended) {
+        alert("You cannot reduce budget lower than spended");
+      } else {
+        state.budget = action.payload;
+      }
       return {
         ...state,
       };
@@ -36,10 +67,12 @@ const initialState = {
     { id: "Department", nameDPT: "Marketing", quantity: 0 },
     { id: "Finance", nameDPT: "Finance", quantity: 0 },
     { id: "Sales", nameDPT: "Sales", quantity: 0 },
-    { id: "Human Resource", nameDPT: "Marketing", quantity: 0 },
+    { id: "Human Resource", nameDPT: "Human Resources", quantity: 0 },
     { id: "IT", nameDPT: "IT", quantity: 0 },
   ],
   currency: "$",
+  budget: "",
+  spended: "",
 };
 
 export const AppContext = createContext();
@@ -47,12 +80,20 @@ export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  const budgetSpend = state.allocation.reduce((currentSpend, item) => {
+    return currentSpend + item.quantity;
+  }, 0);
+
+  state.spended = budgetSpend;
+
   return (
     <AppContext.Provider
       value={{
         currency: state.currency,
         dispatch,
         allocation: state.allocation,
+        budget: state.budget,
+        spended: state.spended,
       }}>
       {children}
     </AppContext.Provider>
